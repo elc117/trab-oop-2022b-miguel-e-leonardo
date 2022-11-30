dbb7637acf0951a423e34903543102ae6987a66f
Leonardo Piekala <37380454+Piekala@users.noreply.github.com>
2022-11-30 12:20:40 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,14 +4,14 @@
 //pois o emulador não consegue se conectar com o localhost
 
 //import 'package:flutter/foundation.dart';
-//import 'package:flutter_t1/mysql_db_config.dart';
+//*****************import 'package:flutter_t1/mysql_db_config.dart';
 import 'package:flutter/material.dart';
 import 'telas.dart';
 
 Future<void> main() async {
-//  var conexao = await MysqlDbConfig().connection;
-//  var result = await conexao!.query('SELECT * FROM medicos');
-//  print(result);
+//***************  var conexao = await MysqlDbConfig().connection;
+//***************  var result = await conexao!.query('SELECT * FROM medicos');
+//***************  print(result);
 
   runApp(MaterialApp(
     title: 'Navegação Básica',
